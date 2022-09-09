50e3ad82ceeee8a22c78a284e7947d76394931b0
E-Svensson <90597251+Anvandare1@users.noreply.github.com>
2022-09-09 10:51:04 +0200

    Add project files.

---
+++ Uppgift2.11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift2._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin?");
+            string längdText=Console.ReadLine();
+            double längd = double.Parse(längdText);
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            string längd2Text=Console.ReadLine();
+            double längd2=double.Parse(längd2Text);
+            double skillnad = längd - längd2;
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma");
+        }
+    }
+}
